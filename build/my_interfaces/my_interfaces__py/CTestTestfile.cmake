58fa76a7b824fb55fc96d9284e938b96e8ed9921
ge-quanyi <1678376818@qq.com>
2023-03-08 15:56:17 +0800

    update predict

--- build/my_interfaces/my_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quonone/workspace/dev_ws/src/build/my_interfaces/my_interfaces__py
-# Build directory: /home/quonone/workspace/dev_ws/src/build/my_interfaces/my_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- install/my_interfaces/local/lib/python3.10/dist-packages/my_interfaces/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from my_interfaces.msg._armor import Armor  # noqa: F401
-from my_interfaces.msg._robot_status import RobotStatus  # noqa: F401
-from my_interfaces.msg._send_data import SendData  # noqa: F401
